96c9a0b8324424f6bed70343ddbde5a143bda0b8
chanreykorn-dev <kornchanrey513@gmail.com>
2025-08-25 12:58:23 +0700

    postcss.config fix 2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss';
+import tailwindcssPostcss from '@tailwindcss/postcss';
 import autoprefixer from 'autoprefixer';
 
 // https://vite.dev/config/
@@ -9,7 +9,7 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        tailwindcss(),
+        tailwindcssPostcss(),
         autoprefixer(),
       ],
     },
