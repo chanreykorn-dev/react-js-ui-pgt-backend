57c9d09dc07962bf2dfd2b3b7fe45cb6a86f8107
chanreykorn-dev <kornchanrey513@gmail.com>
2025-08-25 13:57:28 +0700

    fix all project

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import tailwindcssPostcss from '@tailwindcss/postcss';
-// import autoprefixer from 'autoprefixer';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // PostCSS config is handled in postcss.config.js
-})
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
